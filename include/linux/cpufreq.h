9bc9ab0132763feebb1422d1338317204a607ddc
showp1984 <showp1984@gmail.com>
2012-04-02 16:11:32 +0200

    cmdline_gov: Only override the governor 2 times

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -32,6 +32,8 @@ extern bool gpu_busy_state;
 /* cmdline_khz variables */
 extern uint32_t cmdline_maxkhz, cmdline_minkhz;
 extern char cmdline_gov[16];
+extern int cmdline_gov_cnt;
+
 /* check_khz function for cmdline khz parameters */
 extern uint32_t acpu_check_khz_value(unsigned long khz);
  
